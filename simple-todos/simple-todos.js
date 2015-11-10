7419f225f48a2e683c822d616c05183be97c1de8
ashkon91 <ashkon91@gmail.com>
2015-11-10 00:06:18 -0800

    added method and login

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -4,8 +4,19 @@ if (Meteor.isClient) {
 	//This code only runs on the client
 	Template.body.helpers({
 		tasks: function () {
+			if(Session.get("hideCompleted")) {
+				//If hide completed is checked, filter tasks
+				return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+			} else {
 			return Tasks.find({}, {sort: {createdAt: -1}});
-		}	
+			}
+		},
+		hideCompleted: function() {
+			return Session.get("hideCompleted");
+		},
+		incompleteCount: function() {
+			return Tasks.find({checked: {$ne: true}}).count();
+		}
 	});
 	
 	Template.body.events({
@@ -17,18 +28,20 @@ if (Meteor.isClient) {
 			var text = event.target.text.value;
 
 			//Insert a task into a collection
-			Tasks.insert({
-				text: text,
-				createdAt: new Date()
-			});
+			Meteor.call("addTask", text);
+
+			
 			event.target.text.value ="";
+		},
+		"change .hide-completed input": function (event) {
+			Session.set("hideCompleted", event.target.checked);
 		}
 	});
 
 	Template.task.events({
 		"click .toggle-checked": function() {
 			//set the checked property to the opposite of its current value
-			Tasks.update(this._id, {
+			Tasks.update(this._id, { 
 				$set: {checked: ! this.checked}
 			});
 		},
@@ -36,7 +49,33 @@ if (Meteor.isClient) {
 			Tasks.remove(this._id);
 		}
 	});
+
+	Accounts.ui.config({
+		passwordSignupFields: "USERNAME_ONLY"
+	});
 }
+
+Meteor.methods({
+	addTask: function (text) {
+		//Make sure the suer is logged in before inserting a task
+		if(! Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
+		}
+
+		Tasks.insert({
+			text: text,
+			createdAt: new Date(),
+			owner: Meteor.userId(),
+			username: Meteor.user().username
+		});
+	},
+	deleteTask: function (taskId){
+		Tasks.remove(taskId);
+	},
+	setChecked: function (taskId, setChecked) {
+		Tasks.update(taskId, { $set: {checked: setChecked }});
+	}
+})
 
 if (Meteor.isServer) {
   Meteor.startup(function () {

18ef93607627a23c90877c9350cb54a63c43b225
ashkon91 <ashkon91@gmail.com>
2015-11-10 13:10:44 -0800

    has logins now

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,7 +1,20 @@
 Tasks = new Mongo.Collection("tasks");
-
+if (Meteor.isServer) {
+  // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish("tasks", function () {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
+  });
+}
 if (Meteor.isClient) {
 	//This code only runs on the client
+	Meteor.subscribe("tasks");
+
 	Template.body.helpers({
 		tasks: function () {
 			if(Session.get("hideCompleted")) {
@@ -38,15 +51,22 @@ if (Meteor.isClient) {
 		}
 	});
 
+	Template.task.helpers({
+		isOwner: function () {
+			return this.owner === Meteor.userId();
+		}
+	});
+
 	Template.task.events({
 		"click .toggle-checked": function() {
 			//set the checked property to the opposite of its current value
-			Tasks.update(this._id, { 
-				$set: {checked: ! this.checked}
-			});
+			Meteor.call("setChecked", this._id, ! this.checked);
 		},
 		"click .delete": function (){
-			Tasks.remove(this._id);
+			Meteor.call("deleteTask", this._id);
+		},
+		"click .toggle-private": function () {
+			Meteor.call("setPrivate",  this._id, ! this.private);
 		}
 	});
 
@@ -73,9 +93,18 @@ Meteor.methods({
 		Tasks.remove(taskId);
 	},
 	setChecked: function (taskId, setChecked) {
-		Tasks.update(taskId, { $set: {checked: setChecked }});
+		Tasks.update(taskId, { $set: { checked: setChecked } });
+	},
+	setPrivate: function (taskId, setToPrivate) {
+		var task = Tasks.findOne(taskId);
+
+		//Make sure only the task owner can make a task private
+		if (task.owner !== Meteor.userId()) {
+			throw new Meteor.Error("not-authorized");
+		}
+		Tasks.update(taskId, { $set: {private: setToPrivate}});
 	}
-})
+});
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
